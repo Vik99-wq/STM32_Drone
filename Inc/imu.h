be2f89a9a899595e016f059a7160c29b6803e139
Vik99-wq <53102799+Vik99-wq@users.noreply.github.com>
2022-07-19 12:34:26 -0500

    Update imu.h

--- Inc/imu.h
+++ Inc/imu.h
@@ -11,4 +11,20 @@
 double phiHat_deg = 0.0;
 double thetaHat_deg = 0.0;
 
+uint8_t IAM_readBytes(SPI_HandleTypeDef *hspi1, uint8_t* regs, uint8_t *data);
+uint16_t * IAM_readAccelGyro(SPI_HandleTypeDef *hspi1);
+uint8_t IAM_writeBytes(SPI_HandleTypeDef *hspi1, uint8_t addr, uint8_t data);
+void IAM_Init (SPI_HandleTypeDef *hspi1);
+
+void IAM_CSHigh(void);
+void IAM_CSLow(void);
+
+uint8_t IAM_WHOAMI(SPI_HandleTypeDef *hspi1);
+int32_t * complemetaryFilter(uint16_t* accelData, uint16_t* gyroData);
+
+void Phi_pid_init(double phiHat_deg);
+int32_t Phi_pid_run(void);
+void Theta_pid_init(double thetaHat_deg);
+int32_t Theta_pid_run(void);
+
 #endif /* __IMU_H__ */
