ab8034395a086b56c4c253f65bf74bf56f5518f3
Vik99-wq <53102799+Vik99-wq@users.noreply.github.com>
2022-07-19 10:18:39 -0500

    Update esc.c

--- Src/esc.c
+++ Src/esc.c
@@ -3,24 +3,24 @@
 
 //https://dronenodes.com/wp-content/uploads/2018/10/quadcopter-motor-cw-ccw.jpg
 
-void setSpeeds(TIM_HandleTypeDef htim2, uint8_t esc1, uint8_t esc2, uint8_t esc3, uint8_t esc4){
+void setSpeeds(TIM_HandleTypeDef * htim2, uint8_t esc1, uint8_t esc2, uint8_t esc3, uint8_t esc4){
 
 	// start tim2 for pwm
-	HAL_TIM_PWM_START(&htim2, TIM_CHANNEL_1);
-	HAL_TIM_PWM_START(&htim2, TIM_CHANNEL_2);
-	HAL_TIM_PWM_START(&htim2, TIM_CHANNEL_3);
-	HAL_TIM_PWM_START(&htim2, TIM_CHANNEL_4);
+	HAL_TIM_PWM_START(htim2, TIM_CHANNEL_1);
+	HAL_TIM_PWM_START(htim2, TIM_CHANNEL_2);
+	HAL_TIM_PWM_START(htim2, TIM_CHANNEL_3);
+	HAL_TIM_PWM_START(htim2, TIM_CHANNEL_4);
 
 	// put the pwm signal into each tim2's register
-	htim2.Instance->CCR1 = esc1;
-	htim2.Instance->CCR2 = esc2;
-	htim2.Instance->CCR3 = esc3;
-	htim2.Instance->CCR4 = esc4;
+	htim2->Instance->CCR1 = esc1;
+	htim2->Instance->CCR2 = esc2;
+	htim2->Instance->CCR3 = esc3;
+	htim2->Instance->CCR4 = esc4;
 
 }
 
 // get the current tim2 pwm signal strengths
-int getPwm(TIM_HandleTypeDef *htim2, const char *esc) {
+int getPwm(TIM_HandleTypeDef * htim2, const char *esc) {
 
 	if (strcmp(esc, "esc1") == 0)
 		return htim2->Instance->CCR1;
@@ -35,9 +35,14 @@ int getPwm(TIM_HandleTypeDef *htim2, const char *esc) {
 
 }
 
-void setThrottle(TIM_HandleTypeDef htim2, uint8_t throttle){
+void setThrottle(TIM_HandleTypeDef * htim2, uint8_t throttle){
 
 	setSpeeds(htim2, throttle, throttle, throttle, throttle);
 
 }
 
+void tim2Init(TIM_HandleTypeDef *htim2){
+
+	HAL_TIM_PWM_Init(htim2);
+
+}
