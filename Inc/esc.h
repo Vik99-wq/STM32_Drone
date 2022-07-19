140887f449046163a82ec89041ae7da803eec1df
Vik99-wq <53102799+Vik99-wq@users.noreply.github.com>
2022-07-19 12:34:52 -0500

    Update esc.h

--- Inc/esc.h
+++ Inc/esc.h
@@ -3,4 +3,9 @@
 
 #include "main.h"
 
+void setSpeeds(TIM_HandleTypeDef * htim2, uint8_t esc1, uint8_t esc2, uint8_t esc3, uint8_t esc4);
+int getPwm(TIM_HandleTypeDef * htim2, const char *esc);
+void setThrottle(TIM_HandleTypeDef * htim2, uint8_t throttle);
+void tim2Init(TIM_HandleTypeDef *htim2);
+
 #endif /* __ESC_H__ */
