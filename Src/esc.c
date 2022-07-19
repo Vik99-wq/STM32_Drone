2dc85f2ad75c1d03910f3b7e46a82e9a692802a2
Vik99-wq <53102799+Vik99-wq@users.noreply.github.com>
2022-07-18 22:47:26 -0500

    Update esc.c

--- Src/esc.c
+++ Src/esc.c
@@ -1,7 +1,9 @@
 #include "esc.h"
-#include "imu.c"
+#include "stm32f4xx_hal_tim.h"
 
-void setSpeeds(uint8_t esc1, uint8_t esc2, uint8_t esc3, uint8_t esc4){
+//https://dronenodes.com/wp-content/uploads/2018/10/quadcopter-motor-cw-ccw.jpg
+
+void setSpeeds(TIM_HandleTypeDef htim2, uint8_t esc1, uint8_t esc2, uint8_t esc3, uint8_t esc4){
 
 	// start tim2 for pwm
 	HAL_TIM_PWM_START(&htim2, TIM_CHANNEL_1);
@@ -12,26 +14,26 @@ void setSpeeds(uint8_t esc1, uint8_t esc2, uint8_t esc3, uint8_t esc4){
 	// Is pwm signal inverted?
 
 	// put the pwm signal into each tim2's register
-	htim2.Instance->CCR1 = 200 - esc1;
-	htim2.Instance->CCR2 = 200 - esc2;
-	htim2.Instance->CCR3 = 200 - esc3;
-	htim2.Instance->CCR4 = 200 - esc4;
+	htim2.Instance->CCR1 = esc1;
+	htim2.Instance->CCR2 = esc2;
+	htim2.Instance->CCR3 = esc3;
+	htim2.Instance->CCR4 = esc4;
 }
 
 // get the current tim2 pwm signal strengths
-void getPwm(char esc){
-	switch(esc){
-		case "esc1":
-			return htim2.Instance->CCR1;
-		case "esc2":
-			return htim2.Instance->CCR2;
-		case "esc3":
-			return htim2.Instance->CCR3;
-		case "esc4":
-			return htim2.Instance->CCR4;
-		default:
-			return 0;
-	}
+int getPwm(TIM_HandleTypeDef *htim2, const char *esc) {
+	if (strcmp(esc, "esc1") == 0)
+		return htim2->Instance->CCR1;
+	else if (strcmp(esc, "esc2") == 0)
+		return htim2->Instance->CCR2;
+	else if (strcmp(esc, "esc3") == 0)
+		return htim2->Instance->CCR3;
+	else if (strcmp(esc, "esc4") == 0)
+		return htim2->Instance->CCR4;
+	return 1;
 }
 
+void setThrottle(TIM_HandleTypeDef htim2, uint8_t throttle){
+	setSpeeds(htim2, throttle, throttle, throttle, throttle);
+}
 
