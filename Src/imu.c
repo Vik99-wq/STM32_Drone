5d93d2f83c4639e056416a74663ef38b629eb983
Vik99-wq <53102799+Vik99-wq@users.noreply.github.com>
2022-07-18 22:45:18 -0500

    Update imu.c

--- Src/imu.c
+++ Src/imu.c
@@ -1,34 +1,50 @@
 #include "imu.h"
-#include "spi.c"
+#include <math.h>
+#include "spi.h"
+#include "pid.h"
 
-uint8_t IAM_readBytes(uint8_t regs, uint8_t *data) {
-	i = 0;
-	foreach(int *reg, regs)
-		uint8_t txBuf[2] = {(reg | 0x80), 0x00};
-		uint8_t rxBuf[2];
+double phiHat_rad = 0.0;
+double thetaHat_rad = 0.0;
+PID_TypeDef d_PID;
+double phi_PIDOut;
+double theta_PIDOut;
+double phiSetpoint = 0;
+double thetaSetpoint = 0;
+uint16_t accelData[3];
+uint16_t gyroData[3];
+uint8_t raw_data[12];
+uint8_t WHO_AM_I;
 
-		// pull CS low
+// Accel XYZ then Gyro XZY, high register then low register
+uint8_t regs[] = {0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48};
+
+uint8_t IAM_readBytes(SPI_HandleTypeDef *hspi1, uint8_t* regs, uint8_t *data) {
+
+	uint8_t status;
+	uint8_t rxBuf[2];
+	uint8_t txBuf[2];
+
+	for(uint8_t i = 0; i < 12; i++){
+		txBuf[0] = (*(regs + i) | 0x80);
+		txBuf[1] = 0x00;
+
 		IAM_CSLow();
 
 		// data transmit
-		uint8_t status = (HAL_SPI_TransmitReceive(&hspi1, txBuf, rxBuf, 2, HAL_MAX_DELAY) == HAL_OK);
+		status = (HAL_SPI_TransmitReceive(&hspi1, txBuf, rxBuf, 2, HAL_MAX_DELAY) == HAL_OK);
 
-		// pull CS high
 		IAM_CSHigh();
 		// save data to given pointer
-		*data[i] = rxBuf[1];
-		i++;
+		*(data + i) = rxBuf[1];
+	}
 
-	// 0 if error, 1 if good
+	// 1 if error, 0 if good
 	return status;
 
 }
 
-// Accel XYZ then Gyro XZY, high register then low register
-regs = {0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48};
-
-void IAM_readAccelGyro(void){
-	IAM_readBytes(regs, &raw_data);
+uint16_t * IAM_readAccelGyro(SPI_HandleTypeDef *hspi1){
+	IAM_readBytes(hspi1, regs, &raw_data);
 	// goes XYZ
 
 	// Add the low and high byte to the accel variables
@@ -56,47 +72,110 @@ void IAM_readAccelGyro(void){
 	accelData[0] /= 4096;
 	accelData[1] /= 4096;
 	accelData[2] /= 4096;
+
+	uint16_t retVal[2];
+	retVal[0] = accelData;
+	retVal[1] = gyroData;
+	return retVal;
 }
 
-uint8_t IAM_writeBytes(uint8_t addr, uint8_t data){
+uint8_t IAM_writeBytes(SPI_HandleTypeDef *hspi1, uint8_t addr, uint8_t data){
 
 	uint8_t txBuf[2] = {addr, data};
 
-	// pull CS low
 	IAM_CSLow();
 
 	// data transmit
-	uint8_t status = (HAL_SPI_Transmit(&hspi1, txBuf, 2, HAL_MAX_DELAY) == HAL_OK);
+	uint8_t status = (HAL_SPI_Transmit(hspi1, txBuf, 2, HAL_MAX_DELAY) == HAL_OK);
 
-	// pull CS high
 	IAM_CSHigh();
 
 	return status;
 }
 
-void IAM_Initialize (void){
+void IAM_Initialize (SPI_HandleTypeDef *hspi1){
 	// Set the PWR_MGMT_1 register (6B hex) bits as 0x01 to activate the gyro
-	IAM_writeBytes(0x6B, 0x01);
+	IAM_writeBytes(hspi1, 0x6B, 0x01);
+	HAL_Delay(10);
 
 	// Set the SMPLRT_DIV register (19 hex) bits as 0x09 to divide sample rate by 1khz to 100hz
-	IAM_writeBytes(0x19, 0x09);
+	IAM_writeBytes(hspi1, 0x19, 0x09);
+	HAL_Delay(10);
 
 	// Set the GYRO_CONFIG register (1B hex) bits as 00001000 (500dps full scale)
-	IAM_writeBytes(0x1B, 0b00001000);
+	IAM_writeBytes(hspi1, 0x1B, 0b00001000);
+	HAL_Delay(10);
 
 	// Set the ACCEL_CONFIG register (1C hex) bits as 00010000 (8g full scale)
-	IAM_writeBytes(0x1c, 0b00010000);
+	IAM_writeBytes(hspi1, 0x1c, 0b00010000);
+	HAL_Delay(10);
 }
 
+// pull CS high
 void IAM_CSHigh(void) {
-	HAL_GPIO_WritePin(GPIOA, SPI1_IMU_NCS_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, IMU_NCS_Pin, GPIO_PIN_SET);
 }
 
+// pull CS low
 void IAM_CSLow(void) {
-	HAL_GPIO_WritePin(GPIOA, SPI1_IMU_NCS_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, IMU_NCS_Pin, GPIO_PIN_RESET);
 }
 
-uint8_t IAM_WHOAMI(void) {
+uint8_t IAM_WHOAMI(SPI_HandleTypeDef *hspi1) {
 	// Read the WHO_AM_I register (75 hex) byte to access the 8-bit device ID
-	IAM_readBytes(0x75, &WHO_AM_I);
+	IAM_readBytes(hspi1, 0x75, &WHO_AM_I);
+}
+
+int32_t * complemetaryFilter(uint16_t accelData, uint16_t gyroData){
+	// Estimate angles using accelerometer measurements
+	double phiHat_acc_rad = atanf(accelData[1] / accelData[2]);
+	double thetaHat_acc_rad = asinf(accelData[0] / g);
+
+	// Transform body rates to Euler rates
+	double phiDot_rps = gyroData[0] + tanf(thetaHat_rad) * (sinf(phiHat_rad) * gyroData[1] + cosf(phiHat_rad) * r_rps);
+
+	double thetaDot_rps = cosf(phiHat_rad) * gyroData[1] - sinf(phiHat_rad) * r_rps;
+
+
+	phiHat_rad = COMP_FILT_ALPHA * phiHat_acc_rad + (1.0f - COMP_FILT_ALPHA) * (phiHat_rad + (SAMPLE_TIME_MS_USB / 1000.0f) * phiDot_rps);
+
+	thetaHat_rad = COMP_FILT_ALPHA * thetaHat_acc_rad + (1.0f - COMP_FILT_ALPHA) * (thetaHat_rad + (SAMPLE_TIME_MS_USB / 1000.0f) * thetaDot_rps);
+
+	// convert Phi hat and Theta Hat from Radians to Degrees
+	phiHat_deg = phiHat_rad * RAD_TO_DEG;
+	thetaHat_deg = thetaHat_rad * RAD_TO_DEG;
+
+	// create temp array to return phiHat and thetaHat in degrees
+	int32_t tempArr[2];
+	tempArr[0] = phiHat_deg;
+	tempArr[1] = thetaHat_deg;
+	return tempArr;
+}
+
+void Phi_pid_init(double phiHat_deg){
+	PID(&d_PID, &phiHat_deg, &phi_PIDOut, &phiSetpoint, 2, 5, 1, _PID_P_ON_E, _PID_CD_DIRECT);
+	PID_SetMode(&d_PID, _PID_MODE_AUTOMATIC);
+	PID_SetSampleTime(&d_PID, 10);
+	PID_SetOutputLimits(&d_PID, -100, 100);
+}
+
+int32_t Phi_pid_run(void){
+	// run the Phi PID iteration
+	PID_Compute(&d_PID);
+	HAL_Delay(10);
+	return phi_PIDOut;
+}
+
+void Theta_pid_init(double thetaHat_deg){
+	PID(&d_PID, &thetaHat_deg, &theta_PIDOut, &thetaSetpoint, 2, 5, 1, _PID_P_ON_E, _PID_CD_DIRECT);
+	PID_SetMode(&d_PID, _PID_MODE_AUTOMATIC);
+	PID_SetSampleTime(&d_PID, 10);
+	PID_SetOutputLimits(&d_PID, -100, 100);
+}
+
+int32_t Theta_pid_run(void){
+	// run the Theta PID iteration
+	PID_Compute(&d_PID);
+	HAL_Delay(10);
+	return theta_PIDOut;
 }
