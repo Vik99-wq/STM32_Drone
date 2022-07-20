5ea888818349698f02a51176ab1dbc810e3e1a3a
Vik99-wq <53102799+Vik99-wq@users.noreply.github.com>
2022-07-19 22:11:03 -0500

    Update imu.h

--- Inc/imu.h
+++ Inc/imu.h
@@ -8,23 +8,16 @@
 #define RAD_TO_DEG 57.2957795131f
 #define COMP_FILT_ALPHA 0.0500000000f
 
-double phiHat_deg = 0.0;
-double thetaHat_deg = 0.0;
 
-uint8_t IAM_readBytes(SPI_HandleTypeDef *hspi1, uint8_t* regs, uint8_t *data);
 uint16_t * IAM_readAccelGyro(SPI_HandleTypeDef *hspi1);
-uint8_t IAM_writeBytes(SPI_HandleTypeDef *hspi1, uint8_t addr, uint8_t data);
 void IAM_Init (SPI_HandleTypeDef *hspi1);
 
-void IAM_CSHigh(void);
-void IAM_CSLow(void);
-
 uint8_t IAM_WHOAMI(SPI_HandleTypeDef *hspi1);
-int32_t * complemetaryFilter(uint16_t* accelData, uint16_t* gyroData);
+void complemetaryFilter(uint16_t* accelData, uint16_t* gyroData);
 
-void Phi_pid_init(double phiHat_deg);
+void Phi_pid_init();
 int32_t Phi_pid_run(void);
-void Theta_pid_init(double thetaHat_deg);
+void Theta_pid_init();
 int32_t Theta_pid_run(void);
 
 #endif /* __IMU_H__ */
